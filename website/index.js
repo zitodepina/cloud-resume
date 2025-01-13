6acea7f57dd0affda11cff23d29e641cba32c535
Zito <a_depina@comcast.net>
2025-01-13 17:03:42 -0500

    modify index.js for error checking

--- website/index.js
+++ website/index.js
@@ -1,9 +1,16 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch(
-        "https://cv5w5jmhloyiqm7euhsl5724l40ghbbc.lambda-url.us-east-1.on.aws/"
-    );
-    let data = await response.json();
-    counter.innerHTML = `👀 Views: ${data}`;
+    try{
+        let response = await fetch(
+            "https://cv5w5jmhloyiqm7euhsl5724l40ghbbc.lambda-url.us-east-1.on.aws/"
+        );
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        let data = await response.json();
+        counter.innerHTML = `👀 Views: ${data}`;
+        } catch (error) {
+        console.error('Error in updateCounter:', error);
+      }
 }
 updateCounter();
