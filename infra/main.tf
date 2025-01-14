cb0b2fae8867a4ee44e9ff512b7b29c73bd4a5e9
Zito <a_depina@comcast.net>
2025-01-14 16:10:14 -0500

    update phyton version to 3.12

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "myfunc" {
   function_name    = "myfunc"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "func.lambda_handler"
-  runtime          = "python3.8"
+  runtime          = "python3.12"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
