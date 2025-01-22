b533afdff4cc5ba15d07ec5f4844e4ab595c81e0
Zito <a_depina@comcast.net>
2025-01-22 17:34:22 -0500

    refactor func.py

--- infra/lambda/func.py
+++ infra/lambda/func.py
@@ -1,8 +1,47 @@
 import json
 import boto3
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('cloud-resume')
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+
+#dynamodb = boto3.resource('dynamodb')
+#table = dynamodb.Table('cloud-resume')
+
+def get_table_resource():
+    logging.info("Getting Table Ressource...")
+    dynamodb_resource = boto3.resource("dynamodb")
+    table_name = os.environ[ENV_TABLE_NAME]
+    return dynamodb_resource.Table(table_name)
+
+
+def get_views():
+    table = get_table_resource()
+
+    logging.info("Getting views...")
+    response = table.get_item(Key={
+        'id':'0'
+    })
+    views = response['Item']['views']
+    return views
+
+def update_views(views):
+    logging.info("Updating views...")
+    views +1
+    response = table.put_item(Item={
+            'id':'0',
+            'views': views
+    })
+    return views
+
+
 def lambda_handler(event, context):
+    views = get_views()
+    update_views(views)
+
+    '''
     print(table.creation_date_time)
     response = table.get_item(Key={
         'id':'0'
@@ -15,3 +54,4 @@ def lambda_handler(event, context):
             'views': views
     })
     return views
+    '''
