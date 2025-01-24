7a553049cf216c237fad276e714ebff5687f6d68
Zito <a_depina@comcast.net>
2025-01-24 15:02:26 -0500

    refactor lambda function

--- infra/lambda/func.py
+++ infra/lambda/func.py
@@ -5,53 +5,36 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
-
-#dynamodb = boto3.resource('dynamodb')
-#table = dynamodb.Table('cloud-resume')
-
 def get_table_resource():
     logging.info("Getting Table Ressource...")
-    dynamodb_resource = boto3.resource("dynamodb")
-    table_name = os.environ[ENV_TABLE_NAME]
-    return dynamodb_resource.Table(table_name)
+    dynamodb = boto3.resource('dynamodb')
+    return dynamodb.Table('cloud-resume')
 
 
-def get_views():
-    table = get_table_resource()
-
+def get_views(table):
+    #table = get_table_resource()
     logging.info("Getting views...")
     response = table.get_item(Key={
         'id':'0'
     })
-    views = response['Item']['views']
-    return views
+    return response['Item']['views']
 
 def update_views(views):
     logging.info("Updating views...")
-    views +1
+    table = get_table_resource()
     response = table.put_item(Item={
             'id':'0',
             'views': views
     })
-    return views
 
+def add_views(views):
+    logging.info("Adding views...")
+    views = views + 1
+    return views
 
 def lambda_handler(event, context):
-    views = get_views()
+    table = get_table_resource()
+    views = get_views(table)
+    views = add_views(views)
     update_views(views)
-
-    '''
-    print(table.creation_date_time)
-    response = table.get_item(Key={
-        'id':'0'
-    })
-    views = response['Item']['views']
-    views = views + 1
-    print(views)
-    response = table.put_item(Item={
-            'id':'0',
-            'views': views
-    })
     return views
-    '''
