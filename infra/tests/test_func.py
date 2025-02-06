78cacdd0fbbc7b848768a07a103bce5c6570353c
Zito <a_depina@comcast.net>
2025-02-06 16:04:00 -0500

    fix unit test

--- infra/tests/test_func.py
+++ infra/tests/test_func.py
@@ -50,7 +50,7 @@ def test_lambda_handler_existing_entries(aws_credentials):
     #response = lambda_handler({}, {})
 
     response = get_views(table)
-    assert int(response["Item"]["views"]) == 1
+    assert response == 1
 
 '''
     # Assert that the count is incremented
