6d2c407a43572c259b303627d99b88f3ea03e45a
Zito <a_depina@comcast.net>
2025-02-06 16:50:33 -0500

    fix unit test

--- infra/tests/test_func.py
+++ infra/tests/test_func.py
@@ -51,14 +51,8 @@ def test_lambda_handler_existing_entries(aws_credentials):
     #assert int(response) == 1
         
     response = lambda_handler(TABLE_NAME, {})
-   # Assert that the count is incremented
-    assert response["statusCode"] == 200
-    assert response["body"] == json.dumps({"visits": 2})
-    assert response["headers"]["Content-Type"] == "application/json"
-    assert response["headers"]["Access-Control-Allow-Headers"] == "Content-Type, Origin"
-    assert response["headers"]["Access-Control-Allow-Origin"] == "http://localhost"
-    assert response["headers"]["Access-Control-Allow-Methods"] == "OPTIONS,POST,GET"
-  
+    assert int(response) == 2
+
 
 '''
     # Assert that the count is incremented
