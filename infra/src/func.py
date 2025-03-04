5cbfffe22f84798486b2163b0f3654c2abc7902c
Zito <a_depina@comcast.net>
2025-03-04 17:39:05 -0500

    change to iac

--- infra/src/func.py
+++ infra/src/func.py
@@ -7,8 +7,8 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
-#table = dynamodb.Table('cloud-resume')
-table = dynamodb.Table(os.getenv("DYNAMODB_TABLE"))
+table = dynamodb.Table('cloud-resume')
+#table = dynamodb.Table(os.getenv("DYNAMODB_TABLE"))
 
 #get views from db
 def get_views():
