c9dd83a23e4fca060fe5e42047800c9cd3da24a9
Zito <a_depina@comcast.net>
2025-03-06 15:28:47 -0500

    Modify API gateway1

--- website/index.js
+++ website/index.js
@@ -1,35 +1,18 @@
-/*const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    try{
-        let response = await fetch(
-            "https://cv5w5jmhloyiqm7euhsl5724l40ghbbc.lambda-url.us-east-1.on.aws/"
-        );
-        if (!response.ok) {
-             throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        let data = await response.json();
-        counter.innerHTML = `👀 Views: ${data}`;
-        } catch (error) {
-        console.error('Error in updateCounter:', error);
-      }
-}
-updateCounter();
-*/
-
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     try{
         let response = await fetch(
-            "https://ekpzstb6m9.execute-api.us-east-1.amazonaws.com/prod/visitor", {
+            "https://llljmtwhn5.execute-api.us-east-1.amazonaws.com/prod/visitor", {
             method: 'GET',
         });
         if (!response.ok) {
              throw new Error(`HTTP error! Status: ${response.status}`);
         }
         let data = await response.json();
-        counter.innerHTML = `👀 Views: ${data}`;
+        counter.innerHTML = `👀 Views: ${data.views}`;
         } catch (error) {
         console.error('Error in updateCounter:', error);
       }
 }
+
 updateCounter();
