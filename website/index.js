2fdcf574732d55a31dd8344958d5f8df07762c5e
Zito <a_depina@comcast.net>
2025-03-10 16:10:21 -0400

    Modify index.js to display views

--- website/index.js
+++ website/index.js
@@ -4,11 +4,14 @@ async function updateCounter() {
         let response = await fetch(
             "https://llljmtwhn5.execute-api.us-east-1.amazonaws.com/prod/visitor", {
             method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+              },
         });
         if (!response.ok) {
              throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        let data = await response.json();
+        const data = await response.json();
         counter.innerHTML = `👀 Views: ${data.views}`;
         } catch (error) {
         console.error('Error in updateCounter:', error);
