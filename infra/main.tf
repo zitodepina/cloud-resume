18ea39f186f3e2f009ace83f78e3a5f9dca1a5f2
Zito <a_depina@comcast.net>
2025-03-10 18:14:40 -0400

    Modify index.js to display views

--- infra/main.tf
+++ infra/main.tf
@@ -167,6 +167,7 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id = aws_api_gateway_rest_api.resume_project_gateway.id
   resource_id = aws_api_gateway_resource.views_gateway_resource.id
   http_method = aws_api_gateway_method.views_gateway_method.http_method
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.myfunc.invoke_arn
 }
--- website/index.js
+++ website/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     try{
         let response = await fetch(
-            "https://05p9t7z66j.execute-api.us-east-1.amazonaws.com/prod/visitor", {
+            "https://84qhfs9mr3.execute-api.us-east-1.amazonaws.com/prod/visitor", {
             method: 'GET',
             headers: {
                 "Content-Type": "application/json",
