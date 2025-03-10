1594a120db903317f7a75eeb4ca579822000d963
Zito <a_depina@comcast.net>
2025-03-10 18:02:06 -0400

    Modify index.js to display views

--- infra/main.tf
+++ infra/main.tf
@@ -157,7 +157,7 @@ resource "aws_api_gateway_resource" "views_gateway_resource" {
 resource "aws_api_gateway_method" "views_gateway_method" {
   rest_api_id   = aws_api_gateway_rest_api.resume_project_gateway.id
   resource_id   = aws_api_gateway_resource.views_gateway_resource.id
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
@@ -167,7 +167,6 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id = aws_api_gateway_rest_api.resume_project_gateway.id
   resource_id = aws_api_gateway_resource.views_gateway_resource.id
   http_method = aws_api_gateway_method.views_gateway_method.http_method
-  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.myfunc.invoke_arn
 }
