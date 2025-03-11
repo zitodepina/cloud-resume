ac7dccb1d1e03053ba6af7317d307990cb90900e
Zito <a_depina@comcast.net>
2025-03-11 16:14:09 -0400

    Modify index.js to display views

--- website/index.js
+++ website/index.js
@@ -1,24 +1,21 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    try{
-        let response = await fetch(
-            "https://84qhfs9mr3.execute-api.us-east-1.amazonaws.com/prod/visitor", {
+async function get_visitors() {
+    // call post api request function
+    //await post_visitor();
+    try {
+        let response = await fetch('https://84qhfs9mr3.execute-api.us-east-1.amazonaws.com/prod/visitor', {
             method: 'GET',
-            headers: {
-                "Content-Type": "application/json",
-              },
         });
-        if (!response.ok) {
-             throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        let visitors = await response.json();
-        counter.innerHTML = `👀 Views: 10`;
-        } catch (error) {
-        console.error('Error in updateCounter:', error);
-      }
+        let data = await response.json()
+        document.getElementById("counter-number").innerHTML = data['views'];
+        console.log(data);
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
 }
 
-updateCounter();
+
+get_visitors();
 
 /*
 const counter = document.querySelector(".counter-number");
