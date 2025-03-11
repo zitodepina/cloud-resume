5fc6306efa35aab1562c1f3a55a18a274a606361
Zito <a_depina@comcast.net>
2025-03-11 16:00:17 -0400

    Modify index.js to display views

--- website/index.js
+++ website/index.js
@@ -11,7 +11,30 @@ async function updateCounter() {
         if (!response.ok) {
              throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        const visitors = await response.json();
+        let visitors = await response.json();
+        counter.innerHTML = `👀 Views: ${visitors['views']}`;
+        } catch (error) {
+        console.error('Error in updateCounter:', error);
+      }
+}
+
+updateCounter();
+
+/*
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    try{
+        let response = await fetch(
+            "https://84qhfs9mr3.execute-api.us-east-1.amazonaws.com/prod/visitor", {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+              },
+        });
+        if (!response.ok) {
+             throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        let visitors = await response.json();
         counter.innerHTML = `👀 Views: ${visitors.views}`;
         } catch (error) {
         console.error('Error in updateCounter:', error);
@@ -19,3 +42,4 @@ async function updateCounter() {
 }
 
 updateCounter();
+*/
