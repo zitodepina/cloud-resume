62957bbc8a2044345d985bf3792c076b9102ebf4
Zito <a_depina@comcast.net>
2025-03-13 15:09:21 -0400

    update function for Access-Control-Allow-Origin

--- website/index.js
+++ website/index.js
@@ -6,7 +6,7 @@ async function updateCounter() {
             method: 'GET',
         });
         let data = await response.json()
-        document.getElementById("visitors").innerHTML = `👀 Views: ${data.views}`;
+        document.getElementById("visitors").innerHTML = `👀 Views: ${data}`;
         console.log(data);
         return data;
     } catch (err) {
