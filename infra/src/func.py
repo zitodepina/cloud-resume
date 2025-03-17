d99c6ced2e0a527a882cc29531d8b6c21c3555c8
Zito <a_depina@comcast.net>
2025-03-17 18:07:07 -0400

    update javascript function with versioning

--- infra/src/func.py
+++ infra/src/func.py
@@ -39,9 +39,9 @@ def lambda_handler(event, context):
         'headers': {
             "Content-Type": "application/json",
             'Access-Control-Allow-Origin' : '*',
-            'Access-Control-Allow-Credentials: true',
-            'Access-Control-Allow-Methods: OPTIONS, GET, POST',
-            'Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control',
+            'Access-Control-Allow-Credentials' : 'true',
+            'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+            'Access-Control-Allow-Headers' : 'Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control'
              },
         'body': get_views(table)
         }
