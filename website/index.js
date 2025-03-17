325ad8adc86c8aeb79fe5b0f079d98bf4d01a55d
Zito <a_depina@comcast.net>
2025-03-17 17:33:35 -0400

    update javascript function with versioning

--- website/index.js
+++ website/index.js
@@ -6,6 +6,7 @@ async function updateCounter() {
             method: 'GET',
             headers: {
                 "Content-Type": "application/json",
+                'Access-Control-Allow-Origin' : '*'
               },
         });
         let data = await response.text();
@@ -17,53 +18,3 @@ async function updateCounter() {
     }
 }
 updateCounter();
-
-
-
-/*
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    try{
-        let response = await fetch(
-            "https://84qhfs9mr3.execute-api.us-east-1.amazonaws.com/prod/visitor", {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json",
-              },
-        });
-        if (!response.ok) {
-             throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        let visitors = await response.json();
-        counter.innerHTML = `👀 Views: ${visitors.views}`;
-        } catch (error) {
-        console.error('Error in updateCounter:', error);
-      }
-}
-updateCounter();
-*/
-
-/*
-
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    try{
-        let response = await fetch(
-            "https://84qhfs9mr3.execute-api.us-east-1.amazonaws.com/prod/visitor", {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json",
-              },
-        });
-        if (!response.ok) {
-             throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        let visitors = await response.json();
-        counter.innerHTML = `👀 Views: ${visitors.views}`;
-        } catch (error) {
-        console.error('Error in updateCounter:', error);
-      }
-}
-
-updateCounter();
-*/
