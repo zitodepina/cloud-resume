42afb1102e0d77fc8749dde5f216b533a3c874a6
Zito <a_depina@comcast.net>
2025-04-03 18:20:00 -0400

    pass api gateway parameters

--- infra/src/func.py
+++ infra/src/func.py
@@ -14,15 +14,15 @@ def get_table_resource():
 
 
 #get views from db
-def get_views(table):
+def get_views(table, tableId):
     logging.info("Getting views...")
-    response = table.get_item(Key={'id':os.getenv("ID")})
+    response = table.get_item(Key={'id':tableId})
     if 'Item' in response:
         return response['Item']['views']
     else:
         logging.info("No Views Counter in DynamoDB Table. Creating...")
         updateTableItems(table)
-        response = table.get_item(Key={'id':os.getenv("ID")})
+        response = table.get_item(Key={'id':tableId})
         return response['Item']['views']
 
 #Update table Items
@@ -30,26 +30,28 @@ def updateTableItems(table):
     logging.info("Updating Tabe Items...")
      # Add some items to the table
     item = {
-        'id': os.getenv("ID"),
+        'id': tableId,
         'views': 0
         }
     table.put_item(Item=item)
 
 #
-def update_views(views, table):
+def update_views(views, table, tableId):
     logging.info("Updating views...")
     response = table.update_item(
-        Key={'id':os.getenv("ID")},
+        Key={'id':tableId},
         UpdateExpression='SET #v = :val',
         ExpressionAttributeNames={'#v': 'views'},
         ExpressionAttributeValues={':val': views}
     )
 
 def lambda_handler(event, context):
+    tableId = event['queryStringParameters']['id']
+
     table = get_table_resource()
-    views = get_views(table)
+    views = get_views(table, tableId)
     views = views + 1
-    update_views(views, table)
+    update_views(views, table, tableId)
     
     return {
         'isBase64Encoded': False,
--- website/index.js
+++ website/index.js
@@ -1,7 +1,7 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     try {
-        let response = await fetch('https://upqg49f2a4.execute-api.us-east-1.amazonaws.com/prod/visitor', {
+        let response = await fetch('https://upqg49f2a4.execute-api.us-east-1.amazonaws.com/prod/visitor?id=adp', {
             method: 'GET',
         });
         let data = await response.text();
