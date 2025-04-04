c1395613d4b542250520deba222e94d386d702c5
Zito <a_depina@comcast.net>
2025-04-04 18:46:04 -0400

    pass Id to js

--- website/index.js
+++ website/index.js
@@ -1,6 +1,8 @@
 const counter = document.querySelector(".counter-number");
 
 const idVariable = process.env.TF_VAR_id;
+console.log(idVariable);
+
 
 VISITORS_ENDPOINT = "https://upqg49f2a4.execute-api.us-east-1.amazonaws.com/prod/visitor" + '?' + idVariable;
 
