e38b914fa93980ee7bf8660858737002d3282b64
Zito <a_depina@comcast.net>
2025-05-14 15:31:28 -0400

    remove references to S3 and Cloud Distribution

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,36 +6,42 @@ variable "aws_secret_key" {}
 
 #variable for region
 variable "region" {
-    default = "us-east-1"
+    description = "AWS Region where resources will be deployed"
+    type        = string
 }
 
 #the environment
 variable "environment" {
-  default = "prod"
+    description = "AWS Environment"
+    type        = string
 }
 
 #variable for database to use
 variable "database" {
-    default = "aws_cloud-resume"
+    description = "AWS Database to use"
+    type        = string
 }
 #variable for s3 bucket to use
 variable "bucket_name" {
-    default = "adalbertodepina-12345"
+    description = "AWS S3 Bucket Name"
+    type        = string
 }
 
 #domain name
 variable "domain_name" {
-    default = "adepina.com"
+    description = "Domain Name"
+    type        = string
 }
-
-
 
 #cloudfront zone Id
 variable "CloudFront_zone_id"{
-    default = "Z2FDTNDATAQYW2" // set value for cloudfront distribution https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-route53-recordset-aliastarget.html
+    description = "set value for cloudfront distribution https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-route53-recordset-aliastarget.html"
+    type        = string
 }
+    
 
 #alias
 variable "domain_alias"{
-     default = "adepina.com"
+    description = "Alias Name"
+    type        = string
 }
